dc56ed87598b7fe80b1276a783018cde71b183ce
yuvraj <yuvraj@gmail.com>
2021-10-08 11:05:03 +0530

    gyqwsguygi

---
+++ 10elementsarray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    int number[10];
+    for(j=0;j<=9;j++)
+    {
+        printf("enter %d number ",j);
+        scanf("%d",&number[j]);
+
+    }
+      for(i=0;i<=9;i++)
+        {.
+            printf("number%d=%d\n",i,number[i]);
+        }
+
+
+}
